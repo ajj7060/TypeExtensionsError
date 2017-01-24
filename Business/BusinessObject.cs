51642ae49fe50623d8e9b052d1ea90eff80f3f94
ajj3085 <ajj3085@PLAGUE>
2017-01-24 13:59:53 -0500

    Add project files.

---
+++ Business/BusinessObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Csla;
+
+namespace Business {
+	[Serializable]
+	public class BusinessObject : LogObject<BusinessObject> {
+		public static readonly PropertyInfo<string> SomeProperty = RegisterProperty<string>(x => x.Some);
+		public string Some {
+			get { return GetProperty(SomeProperty); }
+			set { SetProperty(SomeProperty, value); }
+		}
+
+		public static Task<BusinessObject> NewBusinessObjectAsync() {
+			return DataPortal.CreateAsync<BusinessObject>();
+		}
+
+		[RunLocal]
+		protected override void DataPortal_Create() {
+			Some = "some value";
+
+			foreach (var snapshotValue in TakeSnapshot()) {
+				Debug.WriteLine($"{snapshotValue.Key} = {snapshotValue.Value}");
+			}
+		}
+	}
+}
---
+++ Business/LogObjectT.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Csla;
+using Csla.Core;
+using Csla.Reflection;
+
+namespace Business {
+	[Serializable]
+	public abstract class LogObject<T> : BusinessBase<T> where T : LogObject<T> {
+		protected Dictionary<string, object> TakeSnapshot() {
+			var currentType = GetType();
+			var snapshot = new Dictionary<string, object>();
+
+			using (BypassPropertyChecks) {
+				do {
+					if (currentType == null) continue;
+					var props = currentType.GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+					foreach (var prop in props) {
+						var propName = prop.Name.ToLower();
+						var customAttributes = prop.GetCustomAttributes().ToList();
+
+						if (
+							prop.DeclaringType == currentType && 
+							!snapshot.ContainsKey(propName) && 
+							!typeof(IUndoableObject).IsAssignableFrom(prop.PropertyType) &&
+							prop.GetIndexParameters().Length == 0
+						) {
+							snapshot.Add(propName, prop.GetValue(this, null));
+						}
+					}
+
+#if !XAMARIN
+					currentType = currentType.BaseType;
+#else
+					currentType = currentType.BaseType();
+#endif
+
+				} while (currentType != typeof(LogObject<T>));
+			}
+			return snapshot;
+		}
+	}
+}
